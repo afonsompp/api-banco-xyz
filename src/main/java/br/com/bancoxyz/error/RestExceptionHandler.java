34423e2093f922ad986cea1269694fc1a55e14b6
afonso mateus <afonsoprad10@gmail.com>
2021-01-02 14:39:50 -0400

    feat: create ExceptionHandler to handle duplicate data errors

---
+++ src/main/java/br/com/bancoxyz/error/RestExceptionHandler.java
@@ -0,0 +1,25 @@
+package br.com.bancoxyz.error;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ConstraintViolationException e,
+            HttpServletRequest request) {
+        String error = "Email or CPF already exist";
+        HttpStatus status = HttpStatus.CONFLICT;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
