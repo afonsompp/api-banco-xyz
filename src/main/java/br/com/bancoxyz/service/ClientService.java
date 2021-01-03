3e5a39fdcc5a59909fc8e2ef985ce5889d876e10
afonso mateus <afonsoprad10@gmail.com>
2021-01-03 19:09:13 -0400

    feat: add methods to get, put and delete requests

--- src/main/java/br/com/bancoxyz/service/ClientService.java
+++ src/main/java/br/com/bancoxyz/service/ClientService.java
@@ -1,8 +1,14 @@
 package br.com.bancoxyz.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import br.com.bancoxyz.error.exception.ResourceNotFoundException;
 import br.com.bancoxyz.model.Client;
 import br.com.bancoxyz.repository.ClientRepository;
 
@@ -12,7 +18,44 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
+    private String notFoundMsg = "Não possível encontrar o registro com id: ";
+
     public Client insert(Client client) {
         return clientRepository.save(client);
+    }
+
+    public List<Client> findAll() {
+        return clientRepository.findAll();
+    }
+
+    public Client findById(Long id) {
+        Optional<Client> obj = clientRepository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(notFoundMsg, id));
+    }
+
+    public boolean deleteById(Long id) {
+        try {
+            clientRepository.deleteById(id);
+            return true;
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(notFoundMsg, id);
+        }
+    }
+
+    public Client update(Long id, Client obj) {
+        try {
+            Client client = clientRepository.findById(id).get();
+            updateData(client, obj);
+            return clientRepository.save(client);
+        } catch (NoSuchElementException e) {
+            throw new ResourceNotFoundException(notFoundMsg, id);
+        }
+    }
+
+    private void updateData(Client client, Client obj) {
+        client.setName(obj.getName());
+        client.setEmail(obj.getEmail());
+        client.setCpf(obj.getCpf());
+        client.setDateOfBirth(obj.getDateOfBirth());
     }
 }
