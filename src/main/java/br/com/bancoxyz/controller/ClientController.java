ab0ab9f30838d670c0ceda5d3b7b4364e822abf5
afonso mateus <afonsoprad10@gmail.com>
2021-01-03 22:07:47 -0400

    feat: add support to delete client by id

--- src/main/java/br/com/bancoxyz/controller/ClientController.java
+++ src/main/java/br/com/bancoxyz/controller/ClientController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,6 +75,20 @@ public class ClientController {
         return ResponseEntity.ok().body(ClientDTOResponse.parseToDtoResponse(client));
     }
 
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @ApiOperation(value = "${client.delete.value}")
+    @ApiResponses(value = {
+        @ApiResponse(code = 204, message = "Cliente deletado com sucesso."),
+        @ApiResponse(code = 404, message = "Nenhum cliente encontrado"),
+    })
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<ClientDTOResponse> deleteById(
+            @ApiParam(value = "${client.delete.param}", required = true)
+            @PathVariable Long id) {
+
+        clientService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 
     @PutMapping(value = "/{id}")
     @ApiOperation(value = "${client.put.value}")
